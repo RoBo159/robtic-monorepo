e44eae687d5bb50d6927d9732811dcebd2e71eb0
RoBoCRAFTYT01 <fa83734@gmail.com>
2025-11-21 15:45:19 +0100

    create new types
    each prisma table have type
    
    strcture data type

--- packages/shared/main.ts
+++ packages/shared/main.ts
@@ -1,4 +1,26 @@
-export * from "./utils/logger";
-export * from "./types/client.d";
-export * from "./types/embed.d";
-export * from "./types/prisma.d";
+// --- utils ---
+
+// --- data types ---
+import type { CommandType } from "./types/client";
+import type { EmbedData, Field, channelProps, LogsChannelProps } from "./types/discord";
+import type { accessType, ticketBy, ticketStatus } from "./types/ticket";
+
+// --- database type
+import type { PanelProps, TicketProps, TicketUpdate, FormDataProps } from "./types/prisma";
+
+export {
+    // --- types :
+    CommandType,
+    EmbedData,
+    Field,
+    channelProps,
+    LogsChannelProps,
+    accessType,
+    ticketBy,
+    ticketStatus,
+
+    // --- database :
+    PanelProps, TicketProps, TicketUpdate, FormDataProps,
+
+    // --- utils :
+}
--- packages/shared/types/prisma.d.ts
+++ packages/shared/types/prisma.d.ts
@@ -1,5 +1,16 @@
-import { Panel } from "@robo/db";
+import { Panel, Ticket, TicketPanel, FormData } from "@robo/db";
 
 export interface PanelProps extends Panel {
+    ticketPanels?: TicketPanel[];
+}
+
+export interface TicketProps extends TicketPanel {
+    tickets?: Ticket[];
+}
+
+export type TicketUpdate = Omit<TicketProps, "id" | "panelId" | "panel">;
+
+export interface FormDataProps extends FormData {
+    questions?: QuestionProps[];
     ticketPanels?: TicketPanel[];
 }
